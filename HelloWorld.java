8c287531fa7003e621d3765a84db59438d616373
Santosh Sridhar <santosh.sridhar1@wipro.com>
2017-05-03 16:56:59 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld
+{
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello World is said thru job of Jenkins :)");
+
+for(int i=1;i<=10;i++)
+{
+System.out.println("Huray");
+	}
+}
+}
